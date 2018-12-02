eac42d25c29f19a1c4768bba35a0280f82563ae7
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2018-12-01 22:26:47 -0600

    Storyboard setup completed

---
+++ Shopping List/View Controllers/SendOrderViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SendOrderViewController.swift
+//  Shopping List
+//
+//  Created by Ilgar Ilyasov on 12/1/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class SendOrderViewController: UIViewController {
+    
+    @IBOutlet weak var shoppingCartInfoLabel: UILabel!
+    @IBOutlet weak var customerNameTextField: UITextField!
+    @IBOutlet weak var customerDeliveryAddressTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func sendOrderButtonTapped(_ sender: Any) {
+        
+    }
+}
---
+++ Shopping List/Views/ShoppingListCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ShoppingListCollectionViewCell.swift
+//  Shopping List
+//
+//  Created by Ilgar Ilyasov on 12/1/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingListCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var itemImageView: UIImageView!
+    @IBOutlet weak var itemName: UILabel!
+    @IBOutlet weak var itemStatus: UILabel!
+    
+    
+}
